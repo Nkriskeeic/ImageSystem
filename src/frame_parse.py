0f00b0005de8ce07286d8bf2c921fd8926c49c2c
Shun Naka <kreski.linux.sea@gmail.com>
2017-07-01 23:08:15 +0900

    処理を通して一定の部分を抽出

--- src/frame_parse.py
+++ src/frame_parse.py
@@ -1,24 +1,24 @@
 import cv2
 from os import path as op
 import numpy as np
-from libs.Rectangle import Rectangle
-
-
-
+from libs.CharacterArea import CharacterArea
 
 if __name__ == '__main__':
+    # 定義
     IMAGE_DIR = op.normpath(
         op.abspath(op.join(op.dirname(__file__), '../images/')))
     # apple = cv2.imread(IMAGE_DIR + '/apple.png', cv2.IMREAD_ANYCOLOR)
     # banana = cv2.imread(IMAGE_DIR + '/banana.png', cv2.IMREAD_ANYCOLOR)
     # chimpanzee = cv2.imread(IMAGE_DIR + '/chimpanzee.png', cv2.IMREAD_ANYCOLOR)
+    LOWER_GREEN = np.array([30, 30, 30])
+    UPPER_GREEN = np.array([90, 255, 255])
+    CARD_SIZE = [480, 360]
+    PTS_DST = np.array([[[CARD_SIZE[0], 0]], [[0, 0]], [[0, CARD_SIZE[1]]], [[CARD_SIZE[0], CARD_SIZE[1]]]])
 
     frame = cv2.imread(IMAGE_DIR + '/test.jpg', cv2.IMREAD_COLOR)
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_green = np.array([30, 30, 30])
-    upper_green = np.array([90, 255, 255])
 
-    mask = cv2.inRange(hsv_frame, lower_green, upper_green)
+    mask = cv2.inRange(hsv_frame, LOWER_GREEN, UPPER_GREEN)
     blur_mask = cv2.GaussianBlur(mask, ksize=(5, 5), sigmaX=2)
     green_frame = cv2.bitwise_and(frame, frame, mask=blur_mask)
 
